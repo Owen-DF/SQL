5bc3b83d3e7e36ced70a2e725418673e9ba2b851
Owen-DF <owenfleischer@gmail.com>
2024-10-24 10:16:24 +0200

    need to keep working on 4.2, Jesus will check

--- triggers.sql
+++ triggers.sql
@@ -14,9 +14,16 @@ end;
 
 
 --4.2 Trigger when registering a game, check that the black and white are part of the club that organized (and that they havent left)
-
+--  NEED TO FINISH
 create or replace trigger gameCheckClub
 before insert
 on Games
 for each row
+when(
+    --check if the white and black are part of the club that registered the tournamentGame. and that neither player has left the club
+    
+)
 begin
+
+
+--4.3
